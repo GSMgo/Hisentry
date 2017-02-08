8e79eb6a790c89817851709f401364f50671759d
gsmgo <gsmgo.mail@gmail.com>
2017-02-08 00:08:10 -0600

    profile view setup

--- db/migrate/20161111044539_create_users.rb
+++ db/migrate/20161111044539_create_users.rb
@@ -1,6 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
+      t.string :username
+      t.string :name
+      t.string :last_name
+      t.int :age
 
       t.timestamps
     end
