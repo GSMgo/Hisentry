2c5200a435dd50e423c464885f265f04e953e335
gsmgo <gsmgo.mail@gmail.com>
2017-02-25 23:38:24 -0600

    added a little style to forms

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   resources :users, :only => [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index' 
-
-
   
 end
  
