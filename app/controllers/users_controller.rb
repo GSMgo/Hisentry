1bd7add686752b98bba088eace2a837347ba42b8
gsmgo <gsmgo.mail@gmail.com>
2017-02-25 21:54:11 -0600

    deleted events controller and fixed strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,48 +1,48 @@
 class UsersController < ApplicationController
 before_action :authenticate_user!
 
-  def index
-    @users = User.all
-  end
+  # def index
+  #   @users = User.all
+  # end
 
   def show
     @user = User.find(params[:id])
   end
 
-  def new
-    @user = User.new
-  end
+  # def new
+  #   @user = User.new
+  # end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  # def edit
+  #   @user = User.find(params[:id])
+  # end
 
-  def create
-    @user = User.new(user_params)
+  # def create
+  #   @user = User.new(user_params)
  
-    if @user.save
-      redirect_to @user
-    else
-      render 'new'
-    end
-  end
+  #   if @user.save
+  #     redirect_to @user
+  #   else
+  #     render 'new'
+  #   end
+  # end
 
-  def update
-    @user = User.find(params[:id])
+  # def update
+  #   @user = User.find(params[:id])
  
-    if @user.update(user_params)
-      redirect_to @user
-    else
-      render 'edit'
-    end
-  end
+  #   if @user.update(user_params)
+  #     redirect_to @user
+  #   else
+  #     render 'edit'
+  #   end
+  # end
 
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
+  # def destroy
+  #   @user = User.find(params[:id])
+  #   @user.destroy
  
-    redirect_to users_path
-  end
+  #   redirect_to users_path
+  # end
 
   private
 
